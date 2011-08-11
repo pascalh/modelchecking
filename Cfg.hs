dba427d119473ecc00e6d4f06e8e1190a6e5fe01
pascalhof <pascal.hof@tu-dortmund.de>
2011-08-11 22:13:35 +0200

    create a kripke structure for control flow graphs of WHILE-language

--- Cfg.hs
+++ Cfg.hs
@@ -4,132 +4,22 @@ module Cfg where
 import Language.While.Abswhile
 import Language.While.ErrM
 import Language.While.Parwhile
-import qualified Data.Graph.Inductive as G
 
 import Test.QuickCheck
 import WhileMisc
 import Kripke
 import CTL
-import GraphUtil
 import DataToGraph
 import PrintKripke (showKripke)
-import Control.Arrow (first)
 import Control.Monad (forM_)
-import Data.Maybe (fromJust)
-
-data Varops = Decl String | Read String | Write String | VLabel String 
-
-instance Show Varops where
-  show (Decl s) = s
-  show (Read s) = s
-  show (Write s) = s
-  show (VLabel s) = s
-
--- * define a datatype which contains the transformation functions 
-
--- |data type for defining cfg-node-label-creations
-data WhileKripkeLabel k 
-  = WhileKripkeLabel 
-  { program :: Program -> KripkeLabel k
-  , stmts   :: Stmts   -> KripkeLabel k
-  , stmt    :: Stmt    -> KripkeLabel k
-  , bexp    :: BExp    -> KripkeLabel k
-  }
-{-
-vars :: WhileKripkeLabel Varops
-vars = WhileKripkeLabel toKripkeLabel toKripkeLabel toKripkeLabel toKripkeLabel 
-
-class TonNode a b where
-  toKripkeLabel :: a -> KripkeLabel b
-
-class Foo a b where
-  getCfg :: WhileKripkeLabel b -> a -> G.Gr (KripkeLabel b) ()
-  getCfg v p = toCfg v undefined p undefined undefined
-
-  toCfg :: WhileKripkeLabel b -> Int -> a -> Int -> G.Gr (KripkeLabel b) () -> G.Gr (KripkeLabel b) ()
-
-instance Foo Program b where
-  toCfg v _ (Program ss) _ _ =   
-    let g     = G.empty 
-        [i,t] = G.newNodes 2 g
-        h     = G.insNodes [(t,Terminal),(i,Initial)] g
-    in toCfg v i ss t h
 
 
-instance Foo Stmts b where
-  toCfg v i (StmtsE s  ) t g = toCfg v i s t g
-  toCfg v i (Stmts s ss) t g = 
-    let b = head $ G.newNodes 1 g in 
-      toCfg v b ss t $ toCfg v i s b $ G.insNode (b,toKripkeLabel $ Stmts s ss ) g
 
-instance Foo Stmt b where
-  toCfg v i s t g = 
-    case s of
-      (SIf _ _)    -> insCond v i t s g
-      (SWhile _ _) -> insCond v i t s g
-      _            -> insBetween i t (stmt v $ s) g
-
-
--- ks: a -> Varops
-instance TonNode Program Varops where
-  toKripkeLabel _ = (Label,[VLabel []])
-
-instance TonNode Stmt Varops where
-  toKripkeLabel (SDecl (Ident i))     = (Normal,[Decl i])
-  toKripkeLabel (SAssign (Ident i) _) = (Normal,[Write i])
-  toKripkeLabel (SWhile _ _)          = (Label, [VLabel "While"])
-  toKripkeLabel (SIf _ _)             = (Label, [VLabel "If"])
-
-instance TonNode Stmts Varops where
-  toKripkeLabel _ = (Label,[VLabel []])
-
-instance TonNode BExp Varops where
-  toKripkeLabel _ = (Label,[VLabel []])
-  
-
-
-insCond :: WhileKripkeLabel b -> G.Node -> G.Node -> Stmt 
-        -> G.Gr (KripkeLabel b) () -> G.Gr (KripkeLabel b) ()
-insCond v i t s@(SWhile _ ss) g =     
-  let [x,y] = G.newNodes 2 g
-      h = G.insEdges [(y,x,()),(x,y,()),(i,x,()),(y,t,())]  
-        $ G.insNodes [(x,stmt v s),(y,stmt v s)] g 
-  in toCfg v x ss y h
-
-insCond v i t s@(SIf _ ss) g =     
-  let [x,y] = G.newNodes 2 g
-      h = G.insEdges [(x,y,()),(i,x,()),(y,t,())]  
-        $ G.insNodes [(x,stmt v s),(y,stmt v s)] g 
-  in toCfg v x ss y h
-
-
--- simple inserting nodes
-insBetween :: G.Node -> G.Node -> KripkeLabel a 
-           -> G.Gr (KripkeLabel a) () -> G.Gr (KripkeLabel a) ()
-insBetween i t v g = 
-  let n = head $ G.newNodes 1 g in
-  G.insEdge (i,n,()) $ G.insEdge (n,t,()) $ G.insNode (n,v) g
--}
-
--- |removes all empty tags from given graph
--- TODO remove all states marked with Label
-prune :: G.DynGraph g => g (KripkeLabel a) () -> g (KripkeLabel a) ()
-prune g = prune' (G.nodes g) g where
-  prune' []     g = g
-  prune' (n:ns) g = let ((_,_,l,_),_) = first fromJust $ G.match n g in 
-    case l of
-      (Label,_) ->  prune' ns (removeNode n g)
-      _     ->  prune' ns g
-  
--- * testing values
-
-{-
 test :: IO ()
-test = mapM_ 
-  (\x -> let cfg = prune $ (getCfg::WhileKripkeLabel Varops -> Program -> G.Gr (KripkeLabel Varops) ())vars x in 
-    putStrLn "----" >> print ( pretty x) >> print cfg) 
-    programs 
--}
+test = forM_ programs $ \p ->  
+  let cfg = varCFG p in 
+  putStrLn "----" >> print (pretty p) >> showKripke cfg 
+
 test2 :: IO ()
 test2 = forM_ programs $ \p -> do
   let (k::KripkeGr String) = dataToKripke p 
@@ -183,8 +73,9 @@ test2_CTL = forM_ programs $ \p -> do
   let (k::KripkeGr String) = dataToKripke p 
   print (pretty p) 
   print "------"
-  showKripke k 
+  let f = EG $ Disj (AP "[]") (AP "(:)")
+  print $ eval k f
   print "################################"
-  print $ eval k $ EG $ Disj (AP "[]") (AP "(:)")
+  showKripke k
   --print $ eval k (Conj (AP "(:)") (EX $ AP "'f'"))
 
--- WhileMisc.hs
+++ WhileMisc.hs
@@ -7,10 +7,14 @@ import Language.While.ErrM
 import Language.While.Parwhile
 
 import Data.DeriveTH
-import Data.Generics 
+import Data.Generics hiding (empty) 
 
 import Test.QuickCheck
 
+import Language.While.Abswhile
+import Language.While.ErrM
+import Language.While.Parwhile
+import Kripke
 
 instance Read Program where
   readsPrec _ s = 
@@ -94,4 +98,103 @@ arStmts n
   |n<=0 = liftM StmtsE (arStmt (n-1))
   |n>0  = liftM2 Stmts (arStmt (n-1)) (arStmts (n-1))
  
+-- * creating control-flow graphs for WHILE
 
+varCFG :: Program -> KripkeGr Varops
+varCFG = getCfg vars 
+
+data Varops = Decl String | Read String | Write String | VLabel String 
+
+instance Show Varops where
+  show (Decl s) = "Decl "++s
+  show (Read s) = "Read "++s
+  show (Write s) = "Write "++s
+  show (VLabel s) = s
+
+-- * define a datatype which contains the transformation functions 
+
+-- |data type for defining cfg-node-label-creations
+data WhileKripkeLabel 
+  = WhileKripkeLabel 
+  { program :: Program -> Varops 
+  , stmts   :: Stmts   -> Varops
+  , stmt    :: Stmt    -> Varops
+  , bexp    :: BExp    -> Varops
+  }
+
+vars :: WhileKripkeLabel 
+vars = WhileKripkeLabel toKripkeLabel toKripkeLabel toKripkeLabel toKripkeLabel 
+
+class TonNode a where
+  toKripkeLabel :: a -> Varops 
+
+class Foo a where
+  getCfg :: WhileKripkeLabel -> a -> KripkeGr Varops 
+  getCfg v p = toCfg v undefined p undefined undefined
+
+  toCfg :: WhileKripkeLabel -> Int -> a -> Int -> KripkeGr Varops ->KripkeGr Varops 
+
+instance Foo Program where
+  toCfg v _ (Program ss) _ _ =   
+    let g     = empty 
+        [i,t] = newNodes 2 g
+        h     = addStateWithLabel t (VLabel []) $
+                addInitState i g
+    in toCfg v i ss t h
+
+
+instance Foo Stmts where
+  toCfg v i (StmtsE s  ) t g = toCfg v i s t g
+  toCfg v i (Stmts s ss) t g = 
+    let b = head $ newNodes 1 g in 
+      toCfg v b ss t $ toCfg v i s b $ 
+        addStateWithLabel b (VLabel []) g -- (toKripkeLabel $ Stmts s ss ) g
+
+instance Foo Stmt where
+  toCfg v i s t g = 
+    case s of
+      (SIf _ _)    -> insCond v i t s g
+      (SWhile _ _) -> insCond v i t s g
+      _            -> insBetween i t (stmt v $ s) g
+
+
+-- ks: a -> Varops
+instance TonNode Program where
+  toKripkeLabel _ = VLabel []
+
+instance TonNode Stmt where
+  toKripkeLabel (SDecl (Ident i))     = Decl i
+  toKripkeLabel (SAssign (Ident i) _) = Write i
+  toKripkeLabel (SWhile _ _)          = VLabel "While"
+  toKripkeLabel (SIf _ _)             = VLabel "If"
+
+instance TonNode Stmts where
+  toKripkeLabel _ = VLabel []
+
+instance TonNode BExp where
+  toKripkeLabel _ = VLabel [] 
+
+
+insCond :: WhileKripkeLabel -> KripkeState -> KripkeState  -> Stmt 
+        -> KripkeGr Varops -> KripkeGr Varops
+insCond v i t s@(SWhile _ ss) g = 
+  let [x,y] = newNodes 2 g
+      h = addRel y x $ addRel x y $ addRel i x $ addRel y t   
+        $ addStateWithLabel x (stmt v s)
+        $ addStateWithLabel y (stmt v s) g 
+  in toCfg v x ss y h 
+
+insCond v i t s@(SIf _ ss) g = 
+  let [x,y] = newNodes 2 g
+      h = addRel x y $ addRel i x $ addRel y t  
+        $ addStateWithLabel x (stmt v s) 
+        $ addStateWithLabel y (stmt v s) g 
+  in toCfg v x ss y h
+
+
+-- simple inserting nodes
+insBetween :: KripkeState -> KripkeState -> Varops 
+           -> KripkeGr Varops -> KripkeGr Varops
+insBetween i t v  g =  
+  let n = head $ newNodes 1 g in 
+  addRel i n $ addRel n t $ addStateWithLabel n v g
